c7e277d62233a1cfc4c62ede868944ce878ed605
ZeLingNing <nzl199851@gmail.com>
2019-01-02 18:45:20 +0800

    更新自动更新功能

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 //app.js
 App({
-  
   onLaunch: function () {
     if (wx.canIUse('getUpdateManager')) {
       const updateManager = wx.getUpdateManager()
       updateManager.onCheckForUpdate(function (res) {
-        console.log('onCheckForUpdate====', res)
-        // 请求完新版本信息的回调
         if (res.hasUpdate) {
-          console.log('res.hasUpdate====')
           updateManager.onUpdateReady(function () {
             wx.showModal({
               title: '更新提示',
               content: '新版本已经准备好，是否重启应用？',
               success: function (res) {
-                console.log('success====', res)
-                // res: {errMsg: "showModal: ok", cancel: false, confirm: true}
                 if (res.confirm) {
-                  // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
                   updateManager.applyUpdate()
                 }
               }
             })
           })
           updateManager.onUpdateFailed(function () {
-            // 新的版本下载失败
             wx.showModal({
               title: '已经有新版本了哟~',
               content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
@@ -32,12 +24,16 @@ App({
           })
         }
       })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
     }
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
@@ -67,5 +63,5 @@ App({
   },
   globalData: {
     userInfo: null
-  },
+  }
 })
