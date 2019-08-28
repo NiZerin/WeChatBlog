3e7274660424caa11cbb8a9412a12cf037fbb702
NiZerin98 <nzl199851@gmail.com>
2019-08-28 10:44:59 +0800

    代码格式化

--- app.js
+++ app.js
@@ -11,28 +11,28 @@
 
 
 App({
-    
-  onLaunch: function () {
+
+  onLaunch: function() {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
     if (wx.canIUse('getUpdateManager')) {
       const updateManager = wx.getUpdateManager()
-      updateManager.onCheckForUpdate(function (res) {
+      updateManager.onCheckForUpdate(function(res) {
         if (res.hasUpdate) {
-          updateManager.onUpdateReady(function () {
+          updateManager.onUpdateReady(function() {
             wx.showModal({
               title: '更新提示',
               content: '新版本已经准备好，是否重启应用？',
-              success: function (res) {
+              success: function(res) {
                 if (res.confirm) {
                   updateManager.applyUpdate()
                 }
               }
             })
           })
-          updateManager.onUpdateFailed(function () {
+          updateManager.onUpdateFailed(function() {
             wx.showModal({
               title: '已经有新版本了哟~',
               content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
@@ -46,19 +46,19 @@ App({
         content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
       })
     }
-    
-    
+
+
   },
-  getUserInfo:function(cb){
+  getUserInfo: function(cb) {
     var that = this
-    if(this.globalData.userInfo){
+    if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
+    } else {
       //调用登录接口
       wx.login({
-        success: function () {
+        success: function() {
           wx.getUserInfo({
-            success: function (res) {
+            success: function(res) {
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
@@ -67,11 +67,11 @@ App({
       })
     }
   },
-  globalData:{
-    userInfo:null,
-    openid:'',
-    isGetUserInfo:false,
-    isGetOpenid:false
+  globalData: {
+    userInfo: null,
+    openid: '',
+    isGetUserInfo: false,
+    isGetOpenid: false
 
   }
 })
